e8af45f1ce045d19e0470ab8d9f82b84d62b8a1b
Iona Smith <iona.smith3@nhs.net>
2024-08-23 16:15:35 +0100

    update to new version toolkit

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
-  serviceName: 'Additional Pensions Calculator',
+  serviceName: 'NHS.UK prototype kit',
 
   // Port to run nodemon on locally
   port: 2000,

31a87da6756dd11d1cc73d2136986894c762542a
SushmitaJ21 <122446961+SushmitaJ21@users.noreply.github.com>
2025-03-21 12:28:27 +0000

    Content changes 1

---
+++ app/views/v2/_routes.js
@@ -0,0 +1,69 @@
+// External dependencies
+const e = require('express');
+const express = require('express');
+const router = express.Router();
+
+// Add your routes here - above the module.exports line
+
+// ========================================================================
+// MAIN ADDITIONAL PENSION CALCULATOR JOURNEY
+// ========================================================================
+
+
+// Start page
+router.post(/start/, (req, res) => {
+    res.redirect('date-of-birth')
+});
+
+// What is your date of birth?
+router.post(/date-of-birth/, (req, res) => {
+    res.redirect('previous-contracts')
+});
+
+// Do you have any previous additional pension contracts?
+router.post(/previous-contract/, (req, res) => {
+
+    const contract = req.session.data['contract']
+
+    if (contract == 'yes') {
+        res.redirect('previous-contracts-amount')
+    } else {
+        res.redirect('additional-pension-amount')
+
+    }
+});
+
+// Enter the amount of your previous additional pension contract
+router.post(/PCA/, (req, res) => {
+    res.redirect('additional-pension-amount')
+});
+
+// How much additional pension would you like to buy?
+router.post(/additional-pension-amount/, (req, res) => {
+    res.redirect('dependant-cover')
+});
+
+// Would you like to add Dependant cover?
+router.post(/dependant-cover/, (req, res) => {
+    res.redirect('payment-option')
+});
+
+// How would you like to pay for your additional pension?
+router.post(/payment-option/, (req, res) => {
+
+    const payment = req.session.data['payment']
+
+    if (payment == 'lumpsum') {
+        res.redirect('result')
+    } else {
+        res.redirect('instalments')
+
+    }
+});
+
+// How long do you want your monthly instalments to last?
+router.post(/instalments/, (req, res) => {
+    res.redirect('result')
+});
+
+module.exports = router;

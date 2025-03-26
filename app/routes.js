d3e8ad52136c69ec46eaa766a79eb00d15a55a5c
SushmitaJ21 <122446961+SushmitaJ21@users.noreply.github.com>
2025-03-26 15:15:20 +0000

    Changes

--- app/routes.js
+++ app/routes.js
@@ -13,5 +13,6 @@ router.post('/clear-session-data/', (req, res) => {
 // =======================================
 
 router.use('/v1', require('./views/v1/_routes'));
+router.use('/v2', require('./views/v2/_routes'));
 
 module.exports = router;

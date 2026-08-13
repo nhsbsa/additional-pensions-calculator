bd2533a8ee16d10ec5e08bf742df86dcdc9d58f6
Your Name <thole@nhsbsa.nhs.uk>
2026-08-13 12:06:51 +0100

    Content update to Payment-Option screen

--- app/routes.js
+++ app/routes.js
@@ -14,5 +14,6 @@ router.post('/clear-session-data/', (req, res) => {
 
 router.use('/v1', require('./views/v1/_routes'));
 router.use('/v2', require('./views/v2/_routes'));
+router.use('/v3', require('./views/v3/_routes'));
 
 module.exports = router;
